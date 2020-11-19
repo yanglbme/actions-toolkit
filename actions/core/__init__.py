227defcc9c07418123b218d9bca52890caf2bfad
yanglbme <szuyanglb@outlook.com>
2020-11-19 15:41:39 +0800

    docs: add code comment

--- actions/core/__init__.py
+++ actions/core/__init__.py
@@ -18,7 +18,17 @@ class ExitStatus(enum.IntEnum):
     """Indicates unsuccessful program completion in a general sense."""
 
 
+# -----------------------------------------------------------------------
+# Variables
+# -----------------------------------------------------------------------
+
 def export_variable(name: str, val):
+    """
+    Sets env variable for this action and future actions in the job
+    :param name: the name of the variable to set
+    :param val: the value of the variable. Non-string values will be converted to a string via json.dumps
+    :return: void
+    """
     converted_val = to_command_value(val)
     os.environ.setdefault(name, converted_val)
 
@@ -32,10 +42,20 @@ def export_variable(name: str, val):
 
 
 def set_secret(secret: str):
+    """
+    Registers a secret which will get masked from logs
+    :param secret: value of the secret
+    :return: void
+    """
     issue_command('add-mask', {}, secret)
 
 
 def add_path(input_path: str):
+    """
+    Prepends inputPath to the PATH (for this action and future actions)
+    :param input_path: value of the path
+    :return: void
+    """
     file_path = os.environ.get('GITHUB_PATH') or ''
     if file_path:
         issue_file_command('PATH', input_path)
@@ -45,6 +65,12 @@ def add_path(input_path: str):
 
 
 def get_input(name: str, required: bool = True) -> str:
+    """
+    Gets the value of an input. The value is also trimmed.
+    :param name: name of the input to get
+    :param required: if required or not
+    :return: string
+    """
     name = f'INPUT_{name.replace(" ", "_").upper()}'
     val = os.environ.get(name) or ''
     if required and not val:
@@ -53,47 +79,103 @@ def get_input(name: str, required: bool = True) -> str:
 
 
 def set_output(name: str, value):
+    """
+    Sets the value of an output.
+    :param name: name of the output to set
+    :param value: value to store. Non-string values will be converted to a string via json.dumps
+    :return: void
+    """
     issue_command('set-output', dict(name=name), value)
 
 
 def set_command_echo(enabled: bool):
+    """
+    Enables or disables the echoing of commands into stdout for the rest of the step.
+    Echoing is disabled by default if ACTIONS_STEP_DEBUG is not set.
+    """
     issue('echo', 'on' if enabled else 'off')
 
 
+# -----------------------------------------------------------------------
+# Results
+# -----------------------------------------------------------------------
+
 def set_failed(message: str):
+    """
+    Sets the action status to failed.
+    When the action exits it will be with an exit code of 1
+    :param message: add error issue message
+    :return: void
+    """
     error(message)
     sys.exit(1)
 
 
 def is_debug() -> bool:
+    """Gets whether Actions Step Debug is on or not"""
     return os.environ.get('RUNNER_DEBUG') == '1'
 
 
 def debug(message: str):
+    """
+    Writes debug message to user log
+    :param message: debug message
+    :return: void
+    """
     issue_command('debug', {}, message)
 
 
 def error(message: str):
+    """
+    Adds an error issue
+    :param message: error issue message. Errors will be converted to string via str()
+    :return: void
+    """
     issue('error', message)
 
 
 def warning(message: str):
+    """
+    Adds an warning issue
+    :param message: warning issue message. Errors will be converted to string via str()
+    :return: void
+    """
     issue('warning', message)
 
 
 def info(message: str):
+    """
+    Writes info to log with console.log.
+    :param message: info message
+    :return: void
+    """
     sys.stdout.write(message + os.linesep)
 
 
 def start_group(name: str):
+    """
+    Begin an output group.
+
+    Output until the next `groupEnd` will be foldable in this group
+    :param name: The name of the output group
+    :return: void
+    """
     issue('group', name)
 
 
 def end_group():
+    """End an output group."""
     issue('endgroup')
 
 
 async def group(name: str, fn):
+    """
+    Wrap an asynchronous function call in a group.
+
+    Returns the same type as the function itself.
+    :param name: The name of the group
+    :param fn: The function to wrap in the group
+    """
     start_group(name)
     try:
         result = await fn()
@@ -102,9 +184,24 @@ async def group(name: str, fn):
     return result
 
 
+# -----------------------------------------------------------------------
+# Wrapper action state
+# -----------------------------------------------------------------------
+
 def save_state(name: str, value):
+    """
+    Saves state for current action, the state can only be retrieved by this action's post job execution.
+    :param name: name of the state to store
+    :param value: value to store. Non-string values will be converted to a string via json.dumps
+    :return: void
+    """
     issue_command('save-state', dict(name=name), value)
 
 
 def get_state(name: str) -> str:
+    """
+    Gets the value of an state set by this action's main execution.
+    :param name: name of the state to get
+    :return: string
+    """
     return os.environ.get(f'STATE_{name}') or ''
--- actions/core/utils.py
+++ actions/core/utils.py
@@ -2,6 +2,11 @@ import json
 
 
 def to_command_value(input_val) -> str:
+    """
+    Sanitizes an input into a string so it can be passed into issueCommand safely
+    :param input_val: input to sanitize into a string
+    :return: string
+    """
     if input_val is None:
         return ''
     if isinstance(input_val, str):
