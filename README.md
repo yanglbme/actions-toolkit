77b52235e66bff44bed0bad5c6ae7fff4872f777
yanglbme <szuyanglb@outlook.com>
2020-11-22 21:57:14 +0800

    feat: initial io package

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ The GitHub Actions ToolKit provides a set of packages to make creating actions e
 
 [![Downloads](https://pepy.tech/badge/actions-toolkit/month)](https://pepy.tech/project/actions-toolkit/month)
 [![Supported Versions](https://img.shields.io/pypi/pyversions/actions-toolkit.svg)](https://pypi.org/project/actions-toolkit)
+[![Pypi Versions](https://img.shields.io/pypi/v/actions-toolkit.svg)](https://pypi.python.org/pypi/actions-toolkit)
 [![Contributors](https://img.shields.io/github/contributors/yanglbme/actions-toolkit.svg)](https://github.com/yanglbme/actions-toolkit/graphs/contributors)
 
 ```python
@@ -12,12 +13,12 @@ The GitHub Actions ToolKit provides a set of packages to make creating actions e
 >>> os.environ['INPUT_NAME'] = 'Actions Toolkit'
 >>> core.get_input('name')
 'Actions Toolkit'
->>> core.error('something got wrong')
-::error::something got wrong
->>> core.info('run successfully')
-run successfully
->>> core.set_failed('error occurred')
-::error::error occurred
+>>> core.error('Something went wrong.')
+::error::Something went wrong.
+>>> core.info('Run successfully.')
+Run successfully.
+>>> core.set_failed('SSL certificates installation failed.')
+::error::SSL certificates installation failed.
 ```
 
 ## Installing Actions Toolkit and Supported Versions
---
+++ actions_toolkit/io/utils.py
@@ -0,0 +1,94 @@
+import asyncio
+import errno
+import logging
+import os
+import platform
+import re
+from functools import wraps, partial
+from pathlib import Path
+from stat import S_ISDIR
+
+IS_WINDOWS = platform.system() == 'Windows'
+
+
+def wrap(func):
+    @wraps(func)
+    async def run(*args, loop=None, executor=None, **kwargs):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        p = partial(func, *args, **kwargs)
+        return await loop.run_in_executor(executor, p)
+
+    return run
+
+
+stat = wrap(os.stat)
+
+lstat = wrap(os.lstat)
+rename = wrap(os.rename)
+remove = wrap(os.remove)
+mkdir = wrap(os.mkdir)
+rmdir = wrap(os.rmdir)
+
+if hasattr(os, "sendfile"):
+    sendfile = wrap(os.sendfile)
+
+
+async def exists(fs_path: str):
+    try:
+        await stat(fs_path)
+    except IOError as e:
+        if e.errno == errno.ENOENT:
+            return False
+        raise e
+    return True
+
+
+async def is_directory(fs_path: str, use_stat: bool = False):
+    stats = await stat(fs_path) if use_stat else await lstat(fs_path)
+    return S_ISDIR(stats.st_mode)
+
+
+async def is_rooted(p: str) -> bool:
+    if not p:
+        raise Exception('is_rooted() parameter "p" cannot be empty')
+
+    if IS_WINDOWS:
+        return p.startswith('\\') or re.match(r'^[A-Z]', p, re.IGNORECASE) is not None
+    return p.startswith('/')
+
+
+async def mkdir_p(fs_path: str, max_depth: int = 1000, depth: int = 1):
+    if not fs_path:
+        raise Exception('a path argument must be provided')
+    fs_path = Path(fs_path).resolve()
+    if depth >= max_depth:
+        return mkdir(fs_path)
+
+    try:
+        await mkdir(fs_path)
+        return
+    except IOError as e:
+        if e.errno == errno.ENOENT:
+            await mkdir_p(str(fs_path), max_depth, depth + 1)
+            await mkdir(fs_path)
+            return
+        try:
+            stats = await stat(fs_path)
+        except Exception as e2:
+            raise e2
+        if not S_ISDIR(stats.st_mode):
+            raise e
+
+
+async def try_get_executable_path(file_path: str, extensions: list[str]):
+    stats = None
+    try:
+        stats = await stat(file_path)
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            logging.log(f'Unexpected error attempting to determine if executable file exists "{file_path}": {e}')
+    if stats and Path(file_path).is_file():
+        if IS_WINDOWS:
+            upper_ext = Path(file_path).suffix.upper()
+            pass
