0d175fec5e8442c660ad6689c93176656d5538fe
Yang Libin <contact@yanglibin.info>
2021-12-15 16:20:47 +0800

    feat: support github context (#4)

---
+++ actions_toolkit/context.py
@@ -0,0 +1,59 @@
+import json
+import os
+from collections import defaultdict
+
+
+class Context:
+    payload: dict
+    event_name: str
+    sha: str
+    ref: str
+    workflow: str
+    action: str
+    actor: str
+    job: str
+    run_number: int
+    run_id: int
+    api_url: str
+    server_url: str
+    graphql_url: str
+    repo: dict
+    issue: dict
+
+    def __init__(self):
+        self.payload = defaultdict()
+        file = os.getenv('GITHUB_EVENT_PATH')
+        if file:
+            with open(file, 'r', encoding='utf-8') as f:
+                self.payload = json.load(f)
+        self.event_name = os.getenv('GITHUB_EVENT_NAME', '')
+        self.sha = os.getenv('GITHUB_SHA', '')
+        self.ref = os.getenv('GITHUB_REF', '')
+        self.workflow = os.getenv('GITHUB_WORKFLOW', '')
+        self.action = os.getenv('GITHUB_ACTION', '')
+        self.job = os.getenv('GITHUB_JOB', '')
+        self.run_number = int(os.getenv('GITHUB_RUN_NUMBER'))
+        self.run_id = int(os.getenv('GITHUB_RUN_ID'))
+        self.api_url = os.getenv('GITHUB_API_URL', 'https://api.github.com')
+        self.server_url = os.getenv('GITHUB_SERVER_URL', 'https://github.com')
+        self.graphql_url = os.getenv('GITHUB_GRAPHQL_URL', 'https://api.github.com/graphql')
+        self.repo = self._repo()
+        self.issue = self._issue()
+
+    def _repo(self) -> dict:
+        if os.getenv('GITHUB_REPOSITORY'):
+            owner, repo = os.getenv('GITHUB_REPOSITORY').split('/')
+            return dict(owner=owner, repo=repo)
+        if self.payload['repository']:
+            return dict(owner=self.payload['repository']['owner']['login'], repo=self.payload['repository']['name'])
+        raise Exception("context.repo requires a GITHUB_REPOSITORY environment variable like 'owner/repo'")
+
+    def _issue(self) -> dict:
+        payload = self.payload
+        actor = payload.get('issue') or payload.get('pull_request') or payload
+        repo = self._repo()
+        return {
+            'owner': repo['owner'],
+            'repo': repo['repo'],
+            'number': actor['number']
+        }
